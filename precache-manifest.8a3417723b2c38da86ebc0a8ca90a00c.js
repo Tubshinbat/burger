037fa11a59b2bb1de9be2ba222b6283ca10b5708
tuvshinbat <tuvshinbat596@gmail.com>
2021-01-06 02:27:44 +0800

    Updates

---
+++ precache-manifest.8a3417723b2c38da86ebc0a8ca90a00c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff0b89ee2b474d507c8b078a2cc6501",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "e545306235310f5b7c53",
+    "url": "/burger/static/css/main.ee8cc054.chunk.css"
+  },
+  {
+    "revision": "06d4d00834102cd353b3",
+    "url": "/burger/static/js/2.11505c3d.chunk.js"
+  },
+  {
+    "revision": "81fbb27446145c382bc1dab229a0d054",
+    "url": "/burger/static/js/2.11505c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e545306235310f5b7c53",
+    "url": "/burger/static/js/main.6aef2f8f.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
